7b2ef0deeadeed581ba97958e0d47114673cd026
Ravi Soares <broccosoaresravi@gmail.com>
2023-11-23 11:22:58 -0300

    adc arquivos

--- Program.java
+++ Program.java
@@ -1,48 +1,44 @@
 package application;
 
-import java.util.Locale;
-import java.util.Scanner;
-import entities.Triangle;
+import entities.Product;
 
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Program {
     
     public static void main(String[] args) {
-        
         Locale.setDefault(Locale. US);
         Scanner sc = new Scanner(System.in);
         
-        Triangle x, y;
-        x = new Triangle();
-        y = new Triangle();
-        
-        System.out.println("Enter the measures of triangle X: ");
-        x.a = sc.nextDouble();
-        x.b = sc.nextDouble();
-        x.c = sc.nextDouble();
-        System.out.println("Enter the measures of triangle Y: ");
-        y.a = sc.nextDouble();
-        y.b = sc.nextDouble();
-        y.c = sc.nextDouble();
-        
-        double p = (x.a + x.b + x.c) / 2;
-        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-        
-        p = (y.a + y.b + y.c) / 2;
-        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
        
+        System.out.println("Enter the product data: ");
+        System.out.print("Name: ");
+        String name = sc.next(); // aqui vai o nome da variavel nao entidade e depois os atributos dela
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
         
-        System.out.printf("Triangle X area: %.2f%n", areaX);
-        System.out.printf("Triangle Y area: %.2f%n", areaY);
+        Product product = new Product(name, price); // Triangle x = new Triangle()
         
-        if (areaX > areaY) {
-           System.out.print("Larger area: X");
-        } else {
-            System.out.print("Larger area: Y");
-        }
+        product.setName("Computer");
+        System.out.println("\nUpdate name: " + product.getName());
+        product.setPrice(1200);
+        System.out.println("Update price: " + product.getPrice());
+        
+        System.out.println("\nProduct data: " + product);            
+        
+        System.out.print("\nEnter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.AddProducts(quantity);
+        
+        System.out.println("\nUpdated data: " + product);
+        
+        System.out.print("\nEnter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.RemoveProducts(quantity);
+        
+        System.out.print("\nUpdated data: " + product);
         
         sc.close();
     }
-    
 }
