43469e7a5715195fddabe6767c9f4dd6101beee5
Ravi Soares <broccosoaresravi@gmail.com>
2023-11-24 20:12:38 -0300

    adc arquivos

--- Program.java
+++ Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import entities.Holder;
+import entities.Product;
 
 import java.util.Locale;
 import java.util.Scanner;
@@ -8,46 +8,36 @@ import java.util.Scanner;
 public class Program {
     
     public static void main(String[] args) {
-        
         Locale.setDefault(Locale. US);
         Scanner sc = new Scanner(System.in);
         
-        Holder holder; //declarado aqui pq tem dois construtores que precisam ser colocados no if // else
+       
+        System.out.println("Enter the product data: ");
+        System.out.print("Name: ");
+        String name = sc.next(); // aqui vai o nome da variavel nao entidade e depois os atributos dela
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
         
-        System.out.print("Enter account number: ");
-        int accountNumber = sc.nextInt();
-        sc.nextLine(); // quando houver nextAlguma coisa vai comer minha linha
-        System.out.print("Enter the account holder: ");
-        String accountHolder = sc.nextLine();
-        System.out.print("Is there na initial deposit (y/n)? ");
-        char answer = sc.next().charAt(0);
+        Product product = new Product(name, price); // Triangle x = new Triangle()
         
-        if (answer == 'y') {
-            System.out.print("Enter initial deposit value: ");
-            double initialDeposit = sc.nextDouble();
-            
-            holder = new Holder(accountNumber, accountHolder, initialDeposit); //deposito inicial é opcional
-            
-        } else {
-            holder = new Holder(accountNumber, accountHolder);
-        }
+        product.setName("Computer");
+        System.out.println("\nUpdate name: " + product.getName());
+        product.setPrice(1200);
+        System.out.println("Update price: " + product.getPrice());
         
-        holder.setAccountHolder("Ravi Soares");
-        System.out.println("\nUpdate account holder: " + holder.getAccountHolder());
+        System.out.println("\nProduct data: " + product);            
         
-        System.out.println("\nAccount data: " + holder);
+        System.out.print("\nEnter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.AddProducts(quantity);
         
-        System.out.print("\nEnter a deposit value: ");
-        double depositValue = sc.nextDouble();
-        holder.deposit(depositValue);
+        System.out.println("\nUpdated data: " + product);
         
-        System.out.println("Update data: " + holder);
+        System.out.print("\nEnter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.RemoveProducts(quantity);
         
-        System.out.print("\nEnter a withdraw value: ");
-        double withdrawValue = sc.nextDouble();
-        holder.withdraw(withdrawValue);
-        
-        System.out.print("Update data: " + holder);
+        System.out.print("\nUpdated data: " + product);
         
         sc.close();
     }
